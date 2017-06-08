188f58d7b620543ce676e6701d4c75b4c0c55e9b
Lee Newson <lnewson@redhat.com>
2017-06-09 09:46:24 +1000

    0.1.5rc1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
 setup(
     name='rpmfile',
     description='Read rmp archive files',
-    version="0.1.4",
+    version="0.1.5rc1",
     author='Sean Ross-Ross',
     author_email='srossross@gmail.com',
     url='https://github.com/srossross/rpmfile',
